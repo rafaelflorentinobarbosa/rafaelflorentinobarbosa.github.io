b8fa81abe6c58b82b719d192436cd5e7e0ff50eb
rafaelflorentinobarbosa <rafael.florentino.barbosa@gmail.com>
2023-08-02 17:01:31 -0300

    ajustes menu, efeitos

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 /* Mudar a cor do menu quando scrollar e da seta quando aparecer*/
 
 // Obter a referência para o elemento que queremos animar.
@@ -34,13 +33,33 @@ window.onload = function () {
     /* Abrir Menu mobile*/
     let btnMenu = document.getElementById("botao-menu-mb");
     btnMenu.onclick = function () {
-        menu.style.display = "block";
+        menu.style.opacity= "1";    
+        menu.style.transition="0.5s";  
+        menu.style.transform="translateX(0rem)";
     };
     /* Fechar Menu mobile*/
     let btnFechar = document.getElementById("btn-close-mb");
     let menu = document.getElementById("menu-mobile");
+    let corpo = document.getElementById("corpo");
 
     btnFechar.onclick = function () {
-        menu.style.display = "none";
+        menu.style.transition="0.5s";  
+        menu.style.transform="translateX(40rem)";
+        menu.style.opacity= "0";  
     };
+
+    /* Fechar o menu depois de ser clicado */
+    menu.onclick = function () {
+        menu.style.transition="0.5s";  
+        menu.style.transform="translateX(40rem)";
+        menu.style.opacity= "0";  
+    };
+
+     /* Fechar o menu se clicar fora dele */
+     corpo.onclick = function () {
+        menu.style.transition="0.5s";  
+        menu.style.transform="translateX(40rem)";
+        menu.style.opacity= "0";  
+    };
+
 }
